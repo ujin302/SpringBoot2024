392e219a42d121d57ffab9e961a20373839e1824
= <=>
2024-11-12 14:31:32 +0900

    Chapter03

---
+++ chapter03jpa/src/main/java/com/example/chapter03jpa/Chapter03jpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.chapter03jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter03jpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter03jpaApplication.class, args);
+	}
+
+}
